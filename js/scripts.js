295f879045e993226ad0d497cc9bf60d01bbfd55
philip putnam <putnamp989@gmail.com>
2017-01-17 14:29:02 -0800

    add content to index.html & scripts.js for user input and output to screen

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,22 @@ $(document).ready(function() {
     event.preventDefault();
 
     $("#stress-form").hide();
-    $("#stress-response").show();
+    $(".initially-hidden").show();
 
     $("input:checkbox[name=warning-signs]:checked").each(function() {
       var warningSignsInput = $(this).val();
-      $("#stress-response").append(warningSignsInput + "<br>");
-
+      $("#stress-response").append("<li>" + warningSignsInput + "</li>" + "<br>");
     });
 
+    $("input:checkbox[name=symptoms]:checked").each(function() {
+      var stressSymptoms = $(this).val();
+      $("#symptom-response").append("<li>" + stressSymptoms + "</li>" + "<br>");
+    });
+
+    $("input:checkbox[name=relievers]:checked").each(function() {
+      var userStressRelief = $(this).val();
+      $("#stress-relievers").append("<li>" + userStressRelief +"</li>" + "<br>");
+    });
 
   });
 });
